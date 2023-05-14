8ad5816db6dcba8e5f9f5414ae1e6bd0dba9df57
Renzo Calma <xeganthy@gmail.com>
2023-05-14 00:03:34 -0300

    Used reusable components instead of using its own

--- src/components/Modals/CreateLobbyModal.tsx
+++ src/components/Modals/CreateLobbyModal.tsx
@@ -1,11 +1,5 @@
 import { MutationTuple, useMutation } from "@apollo/client";
-import CloseIcon from "@mui/icons-material/Close";
-import { IconButton, TextField } from "@mui/material";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import Typography from "@mui/material/Typography";
-import { SxProps, useTheme } from "@mui/material/styles";
 import { KeyboardEvent, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UsrContxt } from "../../App";
@@ -14,6 +8,8 @@ import { NONE_LOBBY_ID } from "../../util/constants";
 import { validateYtUrl } from "../../util/helpers";
 import Lobby from "../Chatter/interface/Lobby";
 import GenericResponse from "../Chatter/interface/response/GenericResponse";
+import OutlinedField from "../InputField/OutlinedField";
+import ModalBase from "./ModalBase";
 
 interface LobbyModalProps {
   opened: boolean;
@@ -25,7 +21,6 @@ interface InputState {
 }
 
 const CreateLobbyModal = ({ opened, handleCloseModal }: LobbyModalProps) => {
-  const theme = useTheme();
   const userContext = useContext(UsrContxt);
   const navigate = useNavigate();
 
@@ -45,11 +40,11 @@ const CreateLobbyModal = ({ opened, handleCloseModal }: LobbyModalProps) => {
   };
 
   const [values, setValues] = useState<InputState>({
-    input: "https://www.youtube.com/watch?v=4WXs3sKu41I",
+    input: "",
     error: false,
   });
 
-  const [addUserToLobbyMutation, addUserToLobbbyMutRes]: MutationTuple<
+  const [addUserToLobbyMutation, addUserToLobbyMutRes]: MutationTuple<
     { addUserToLobby: GenericResponse },
     { lobbyId: string; userId: string }
   > = useMutation(ADD_USER_TO_LOBBY);
@@ -95,7 +90,7 @@ const CreateLobbyModal = ({ opened, handleCloseModal }: LobbyModalProps) => {
     else setValues((values) => ({ ...values, error: true }));
   };
 
-  const onCloseHandler = (_: object, reason: string) => {
+  const onCloseHandler = (_: any, reason: string) => {
     if (
       (reason !== "backdropClick" && reason !== "escapeKeyDown") ||
       userContext.lobbyId !== NONE_LOBBY_ID
@@ -103,91 +98,27 @@ const CreateLobbyModal = ({ opened, handleCloseModal }: LobbyModalProps) => {
       handleCloseModal();
   };
 
-  const style: SxProps = {
-    display: "flex",
-    flexDirection: "column",
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "26px",
-    width: 400,
-    bgcolor: theme.modal?.bgColor,
-    px: 4,
-    py: 2,
-    "&:focus": {
-      outline: "none",
-    },
-  };
-
-  const textFieldSx: SxProps = {
-    margin: "18px 0",
-    ".MuiInputLabel-outlined": {
-      color: theme.common.text.secondary,
-    },
-    input: {
-      color: theme.common.text.secondary,
-    },
-    fieldset: {
-      borderColor: theme.textInput?.borderColor,
-      "&.MuiOutlinedInput-notchedOutline": {
-        borderColor: theme.textInput?.borderColor,
-      },
-      "&.Mui-error": {
-        borderColor: theme.common.text.decline,
-      },
-    },
-    ".MuiFormHelperText-root": {
-      margin: "0 auto",
-      marginTop: "5px",
-    },
-  };
-
-  const closeButtonContainerSx: SxProps = {
-    position: "absolute",
-    top: "5px",
-    right: "8px",
-  };
-
-  const closeButtonSx: SxProps = {
-    color: theme.common.text.secondary,
-    width: "20px",
-  };
-
   return (
-    <Modal open={opened} onClose={onCloseHandler}>
-      <Box sx={style}>
-        {userContext.lobbyId && userContext.lobbyId !== "NONE" && (
-          <Box sx={closeButtonContainerSx}>
-            <IconButton aria-label="Example" onClick={handleCloseModal}>
-              <CloseIcon sx={closeButtonSx} />
-            </IconButton>
-          </Box>
-        )}
-        <Typography
-          id="modal-modal-title"
-          variant="h6"
-          component="h2"
-          color={theme.common.text.secondary}
-        >
-          Create your lobby for you and your friends!
-        </Typography>
-        <TextField
-          error={values.error}
-          helperText={values.error ? "Incorrect entry." : ""}
-          autoComplete="off"
-          sx={textFieldSx}
-          id="outlined-basic"
-          label="Video Url"
-          variant="outlined"
-          onChange={handleChange("input")}
-          onKeyDown={handleKeyDown}
-          defaultValue="https://www.youtube.com/watch?v=4WXs3sKu41I"
-          placeholder="https://www.youtube.com/watch?v=4WXs3sKu41I"
-        />
-        <Button onClick={() => submitHandler()}>Create Lobby!</Button>
-      </Box>
-    </Modal>
+    <ModalBase
+      open={opened}
+      onClose={onCloseHandler}
+      header="Create your room for watching together!"
+      hasCloseButton={Boolean(
+        userContext.lobbyId && userContext.lobbyId !== "NONE"
+      )}
+    >
+      <OutlinedField
+        placeholder={"https://www.youtube.com/watch?v=4WXs3sKu41I"}
+        defaultValue={""}
+        onChange={handleChange("input")}
+        onKeyDown={handleKeyDown}
+        autoComplete="off"
+        label="Video Url"
+        error={values.error}
+        helperText={values.error ? "Incorrect youtube link" : ""}
+      />
+      <Button onClick={() => submitHandler()}>Create Lobby!</Button>
+    </ModalBase>
   );
 };
 
